49f30c7a94c51b5379635dc617fcc34b0e4234ce
zhaoxinDuan <412653689@qq.com>
2021-09-16 15:47:49 +0800

    Update HelloWorld.vue
    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    La La La
+
+La La La
+
+I'm an expert in selling newspapers
+
+Wait till dawn for the newspaper
+
+Go on
+
+Yelling
+
+Today's news is really good
+
+Seven coppers buy two newspapers
+
+La La La
+
+La La La
+
+I'm an expert in selling newspapers
+
+Running all over the street in strong wind and heavy rain
+
+I can't walk well
+
+Slip and fall
+
+The muddy water makes people laugh
+
+Hunger and cold only I know
+
+La La La
+
+La La La
+
+I'm an expert in selling newspapers
+
+Running all over the street with hunger and cold tolerance
+
+be on short commons
+
+can't sleep well
+
+Who does the painful life tell
+
+One day the light will come
   </div>
 </template>
 
